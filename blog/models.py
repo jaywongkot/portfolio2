d1a4f53ada22a2bf5d5106895dd6a2ff7c50a076
Jay Wongkot <onzonstudios@gmail.com>
2020-08-10 11:46:34 -0300

    Completed blog category urls

--- blog/models.py
+++ blog/models.py
@@ -10,11 +10,11 @@ class Post(models.Model):
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
-    Categories = models.ManyToManyField('Category', related_name='posts')
+    categories = models.ManyToManyField('Category', related_name='posts')
 
 
 class Comment(models.Model):
     author = models.CharField(max_length=60)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
+
+from blog.forms import CommentForm
 from blog.models import Post, Comment
-from .forms import CommentForm
 
 
 def blog_index(request):
-    posts = Post.objects.all().order_by('-created_on')
-    context = {
-        "posts": posts,
-    }
+    posts = Post.objects.all().order_by("-created_on")
+    context = {"posts": posts}
     return render(request, "blog_index.html", context)
 
 
@@ -15,7 +14,7 @@ def blog_category(request, category):
     posts = Post.objects.filter(
         categories__name__contains=category
     ).order_by(
-        '-created_on'
+        "-created_on"
     )
     context = {
         "category": category,
@@ -26,22 +25,18 @@ def blog_category(request, category):
 
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
 
     form = CommentForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = Comment(
                 author=form.cleaned_data["author"],
                 body=form.cleaned_data["body"],
-                post=post
+                post=post,
             )
             comment.save()
 
-    comments = Comment.objects.filter(post=post)
-    context = {
-        "post": post,
-        "comments": comments,
-        "form": form,
-    }
+    context = {"post": post, "comments": comments, "form": form}
     return render(request, "blog_detail.html", context)
